4d15918022994f3c4fba178319c988a81a0c0663
towhid <niamaaktar@gmail.com>
2023-02-05 08:59:55 +0600

    all changes has been updated

--- nextlist/components/Navbar.js
+++ nextlist/components/Navbar.js
@@ -14,7 +14,6 @@ const Navbar = () => {
     <div className='bg-gray-300 py-2 text-black flex items-center justify-between gap-8 px-12'>
       <div className='w-20 block object-cover'>
         <FaAdversal className='text-5xl font-bold' />
-         {/* <img src='https://unsplash.it/500/500?image=234'/> */}
       </div>
       <div className="search"></div>
       <div className={`${ubuntu.className} flex gap-4 capitalize font-bold`}>
